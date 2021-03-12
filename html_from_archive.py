4f81495ce6186d550aeefadf36593d48ea7073d0
沉冰浮水 <wdssmq@qq.com>
2021-03-12 14:13:41 +0800

    代码格式化

--- html_from_archive.py
+++ html_from_archive.py
@@ -1,50 +1,53 @@
 import json
 from os import path
 with open("outbox.json", "r") as outbox_file:
-	outbox = json.loads(outbox_file.read())
+    outbox = json.loads(outbox_file.read())
 with open("actor.json", "r") as actor_file:
-	actor = json.loads(actor_file.read())
-#map the outbox down to the actual objects
+    actor = json.loads(actor_file.read())
+# map the outbox down to the actual objects
 statuses = [status.get("object") for status in outbox.get("orderedItems")]
 
 articles = []
-#attachment urls may begin with "/media/" or something else we dont want
+# attachment urls may begin with "/media/" or something else we dont want
 # start with an offset of 1 to avoid checking root for /media or something else wrong
 pathOffset = 1
 
 for status in statuses:
-	#need to ignore objects that arent status dicts
-	if type(status) == type({}):
-		date = status.get("published")
-		summary = status.get("summary")
-		htmlContent = status.get("content")
-		attachments = [attachment.get("url") for attachment in status.get("attachment")]
-		images = ""
-		for imageURL in attachments:
-			# only runs the loop for the first media url in the archive
-			if pathOffset == 1:
-				while not path.exists(imageURL[pathOffset:]) and pathOffset < len(imageURL):
-					pathOffset +=1
+    # need to ignore objects that arent status dicts
+    if type(status) == type({}):
+        date = status.get("published")
+        summary = status.get("summary")
+        htmlContent = status.get("content")
+        attachments = [attachment.get("url")
+                       for attachment in status.get("attachment")]
+        images = ""
+        for imageURL in attachments:
+            # only runs the loop for the first media url in the archive
+            if pathOffset == 1:
+                while not path.exists(imageURL[pathOffset:]) and pathOffset < len(imageURL):
+                    pathOffset += 1
 
-			if imageURL[-4:] == ".mp4" or imageURL[-5:] == ".webm":
-				images += "<video controls muted src='{0}' class='status__image'>There should be a video here.</video>".format(imageURL[pathOffset:])
-			else:
-				images += "<img class='status__image' src='{0}'>".format(imageURL[pathOffset:])
-		if summary:
-			article = "<article class='status'>\
+            if imageURL[-4:] == ".mp4" or imageURL[-5:] == ".webm":
+                images += "<video controls muted src='{0}' class='status__image'>There should be a video here.</video>".format(
+                    imageURL[pathOffset:])
+            else:
+                images += "<img class='status__image' src='{0}'>".format(
+                    imageURL[pathOffset:])
+        if summary:
+            article = "<article class='status'>\
 			<div class='status__date'><span>{0}</span></div>\
 			<details><summary class='status__summary'>{1}</summary>\
 			<div class='status__content'>{2}</div>\
 			<div class='status__media'>{3}</div>\
 			</details>\
 			</article>".format(date, summary, htmlContent, images)
-		else:
-			article = "<article class='status'>\
+        else:
+            article = "<article class='status'>\
 			<div class='status__date'><span >{0}</span></div>\
 			<div class='status__content'>{1}</div>\
 			<div class='status__media'>{2}</div>\
 			</article>".format(date, htmlContent, images)
-		articles.append(article)
+        articles.append(article)
 
 outfile = open("processed_archive.html", "w")
 styleSheet = "<style>\
@@ -75,6 +78,6 @@ outfile.write("</head><body>\
 		<div id='actor-summary'>{2}</div>\
 	</section>".format(actor.get("preferredUsername"), actor.get("name"), actor.get("summary")))
 for article in articles:
-	outfile.write(article)
+    outfile.write(article)
 outfile.write("</body></html>")
 outfile.close()
